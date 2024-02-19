6dc4def28817a4d86e26c2584d8668033820c914
Ashitaka01 <nelsponthieu@gmail.com>
2024-02-19 13:21:11 +0000

    Write a C program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"\n);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
