28c0388ab3b68296cfa2bba35ffbc178d02acbc1
Ashitaka01 <nelsponthieu@gmail.com>
2024-02-20 12:29:45 +0000

    Get a random number and check its last digit, compare it with 5

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <time.h>
 /**
  * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
+ * Get a random number and check its last digit, compare it with 5
  * Return: 0
  */
 int main(void)
