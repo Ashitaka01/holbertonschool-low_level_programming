a09829a63269e2c609b0ee9fafefc0ce39520c98
Ashitaka01 <nelsponthieu@gmail.com>
2024-02-20 15:06:49 +0000

    Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+        char a = '0';
+
+        while (a <= '10')
+        {
+		              
+
+                    putchar(a);
+
+                
+                    a++;
+        }
+
+        putchar('\n');
+        return (0);
+}
