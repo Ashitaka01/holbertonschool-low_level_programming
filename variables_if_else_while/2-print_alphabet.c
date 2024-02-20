b38ddff5f48e499018490cc1f129fe906e4652f3
Ashitaka01 <nelsponthieu@gmail.com>
2024-02-20 10:58:49 +0000

    Write a program that prints the alphabet in lowercase, followed by a new line.

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+        
+	while( a <='z')
+	{
+	         putchar(a);
+	         a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
