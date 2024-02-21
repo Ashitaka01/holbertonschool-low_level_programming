ef475d3c8ffdcd86ef73c29fef7c0862875d4b3b
Ashitaka01 <nelsponthieu@gmail.com>
2024-02-21 09:58:48 +0000

    prints all single digit numbers of base 10, starting from 0, followed by a new line.

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -5,13 +5,11 @@
  */
 int main(void)
 {
-        int = i;
-
-        for (i = 0; i < 10; i++)
-       				 {
-               				 putchar(i + '0');
-       				 }
-
-       			 putchar("\n");
-        		 return (0);
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return (0);
 }
