a88a98aaa86d294757cb57f0e6359397f60b319a
Ashitaka01 <nelsponthieu@gmail.com>
2024-02-21 10:19:51 +0000

    prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'z'
+		while (a >= 'a');
+	{
+		putchar(a);
+		a--;
+	}
+	putchar ('\n');
+	return (0);
+	
+}
