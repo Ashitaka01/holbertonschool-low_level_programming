52e6c986784a1b6e30a46999e5ebe2c6a3a7d47d
Ashitaka01 <nelsponthieu@gmail.com>
2024-02-26 16:19:36 +0000

    Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line.

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * prints 10 times the number,  from 0 to 14
+ * Return: Void
+ */
+void more_numbers(void)
+{
+	int n;
+	int i;
+	
+	for (n = 0 ; n <10 ; n++)
+	{
+		for (i = 0 ; i <= 14 ; i++)
+		{
+		
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
