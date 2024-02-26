a358d02bf88fe021a265d1307f8ccfb7115505d9
Ashitaka01 <nelsponthieu@gmail.com>
2024-02-26 11:09:53 +0000

    Write a function that prints the numbers, from 0 to 9, followed by a new line.

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Return:0
+ */
+void print_numbers(void)
+{
+
+	char n;
+
+	(n = 0; n <= 9)
+	{
+
+		_putchar('n');
+                n++;
+
+	}
+
+	_putchar('\n');
+
+}
