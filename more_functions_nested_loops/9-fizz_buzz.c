9fec5c38a54abb9d85df675e79b21878843faf34
Ashitaka01 <nelsponthieu@gmail.com>
2024-02-27 13:51:13 +0000

    Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main (void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			prinft("%s", "FizzBuzz");
+			printf("%s", "FizzBuzz");
 		else if (i % 3 == 0)
 			printf("%s", "Fizz");
 		else if (i % 5 == 0)
@@ -18,7 +18,7 @@ int main (void)
 		else
 			printf("%d", i);
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 
 }
