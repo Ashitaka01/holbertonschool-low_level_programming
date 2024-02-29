6be86dcd7dcaebf11a689b29818424e68b066c2f
Ashitaka01 <nelsponthieu@gmail.com>
2024-02-29 10:26:47 +0000

    Write a function that prints a string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -5,12 +5,11 @@
  */
 void _puts(char *str)
 {
-        
-        while (*str)
-		
-        {
-                _putchar(*str++);
 
-        }
-                _putchar('\n');
-}       
+	 while (*str)
+	{
+		_putchar(*str++);
+
+	}
+		_putchar('\n');
+}
