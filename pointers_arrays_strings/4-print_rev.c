fc6d9c2460da52df44380a1afc0cdbf9994f8921
Ashitaka01 <nelsponthieu@gmail.com>
2024-02-29 10:52:09 +0000

    Write a function that prints a string, in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - Write a function that prints a string, in reverse
+ * @str: the sring de be print
+ */
+void _puts(char *str)
+{
+	int len = 0, index;
+	
+        while (s[index++])
+		len++;
+
+        for (index = len - 1; index >= 0; index--)
+		_putchar(s[index ]);
+
+		_putchar('\n');
+
+}
