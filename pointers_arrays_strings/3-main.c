5dfa116e9a558a7d3310fea19c8929ec27ee500c
Ashitaka01 <nelsponthieu@gmail.com>
2024-03-03 21:52:35 +0000

    Write a function that compares two strings.

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,19 +1,18 @@
 #include "main.h"
 #include <stdio.h>
-/*
- **
- * _strcmp - compares two strings
- * @s1: string 1
- * @s2: string 2
- * Return: 0
- */
-int _strcmp(char *s1, char *s2)
-{
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
 
-	return (*s1 - *s2);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[] = "Hello";
+    char s2[] = "World!";
+
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
 }
--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ **
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+        while (*s1 && *s2 && *s1 == *s2)
+        {
+                s1++;
+                s2++;
+        }
 
+        return (*s1 - *s2);
+}
