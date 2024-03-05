3588d9359afb49215dda2d692810a6dafedf46b7
root <nelsponthieu@gmail.com>
2024-03-05 09:51:49 +0000

    Write a function that prints the chessboard.

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -16,10 +16,13 @@ void print_chessboard(char (*a)[8])
 		{
 		
 			for (j = 0; j < 8; j++)
+			{
+			
 				putchar(a[i][j]);
 				putchar(' ');
+			}
 		
 		}
 
-		_putchar('\n');
+		putchar('\n');
 }
