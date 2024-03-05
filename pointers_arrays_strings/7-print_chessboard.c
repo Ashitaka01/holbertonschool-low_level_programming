0e4303f52d4b8475bd87937c6a9aecfb7e178c79
root <nelsponthieu@gmail.com>
2024-03-05 10:18:27 +0000

    Write a function that prints the chessboard.

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -8,26 +8,28 @@
  * Return: void
  */
 void print_chessboard(char (*a)[8])
-{ 
-       	int i, j;
-    	char a[8][8] = {
-        {'1', '2', '3', '4', '5', '6', '7', '8'},
-        {'9', 'a', 'b', 'c', 'd', 'e', 'f', 'g'},
-        {'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'},
-        {'p', 'q', 'r', 's', 't', 'u', 'v', 'w'},
-        {'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E'},
-        {'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'},
-        {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'},
-        {'V', 'W', 'X', 'Y', 'Z', '0', '1', '2'}
-    };
+{
 
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            putchar(a[i][j]);
-            putchar(' ');
-        }
-        putchar('\n');
-    }
+                int i, j;
 
-    return 0;
+                for (i = 0; i < 8; i++)
+                {
+
+                        for (j = 0; j < 8; j++)
+                        {
+	
+        	                
+				if (a[i][j] == ' ')
+
+                                	putchar(' ');
+                        }
+			else 
+			{
+				putchar(a[i][j]);
+			
+			}	
+                }
+
+                putchar('\n');
 }
+~ 
